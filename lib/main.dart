c9fb5f9d211f1a1196b865c541b4e1bf86ea5d24
Danel <danel.lora@ironchip.com>
2023-03-20 09:41:44 +0100

    finalizar tarea

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/providers/cart.dart';
+import 'package:flutter_complete_guide/providers/orders.dart';
 import 'package:provider/provider.dart';
 
+import './screens/cart_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
+import './providers/orders.dart';
+import './screens/orders_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,8 +20,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
         providers: [
-          ChangeNotifierProvider.value(value: Products()),
-          ChangeNotifierProvider.value(value: Cart()),
+          ChangeNotifierProvider.value(
+            value: Products(),
+          ),
+          ChangeNotifierProvider.value(
+            value: Cart(),
+          ),
+          ChangeNotifierProvider.value(
+            value: Orders(),
+          ),
         ],
         child: MaterialApp(
           title: 'MyShop',
@@ -29,6 +40,8 @@ class MyApp extends StatelessWidget {
           home: ProductsOverviewScreen(),
           routes: {
             ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
+            CartScreen.routeName: (context) => CartScreen(),
+            OrdersScreen.routeName: (context) => OrdersScreen(),
           },
         ));
   }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -19,7 +19,47 @@ class ProductDetailScreen extends StatelessWidget {
     ).findById(productID);
 
     return Scaffold(
-      appBar: AppBar(title: Text(loadedProducts.title)),
+      appBar: AppBar(
+        title: Text(loadedProducts.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProducts.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProducts.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              width: double.infinity,
+              child: Text(
+                loadedProducts.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
